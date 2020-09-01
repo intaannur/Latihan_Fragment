717e8d6b149b084fb53fe9fcf4f2170e3991d394
intaannur <intaannur09.siswa@smkn4bdg.sch.id>
2020-09-01 12:14:29 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan1fragment/ui/dashboard/DashboardFragment.java
@@ -0,0 +1,28 @@
+package com.example.latihan1fragment.ui.dashboard;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.latihan1fragment.R;
+
+public class DashboardFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        //just change the fragment_dashboard
+        //with the fragment you want to inflate
+        //like if the class is HomeFragment it should have R.layout.home_fragment
+        //if it is DashboardFrgamnet it should have R.layout.fragment dashboard
+        return inflater.inflate(R.layout.fragment_dashboard, null);
+    }
+}
---
+++ app/src/main/java/com/example/latihan1fragment/ui/home/HomeFragment.java
@@ -0,0 +1,23 @@
+package com.example.latihan1fragment.ui.home;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.latihan1fragment.R;
+
+public class HomeFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_home, null);
+    }
+}
